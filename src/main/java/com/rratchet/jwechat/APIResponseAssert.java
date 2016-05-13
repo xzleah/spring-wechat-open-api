7a7516fe0a1763a43f47f2410de0c1f82e858f2f
rex lee <xzleah@foxmail.com>
2016-05-13 13:53:53 +0800

    不格式化在异常信息中的微信返回码

--- src/main/java/com/rratchet/jwechat/APIResponseAssert.java
+++ src/main/java/com/rratchet/jwechat/APIResponseAssert.java
@@ -19,7 +19,7 @@ public class APIResponseAssert {
 				
 				throw new AccessTokenExpiredException(errorMessage);
 			}
-			String formatedErrorMessage = MessageFormat.format("wechat response errcode:{0}, errmsg:{1}", errorCode, errorMessage);
+			String formatedErrorMessage = MessageFormat.format("wechat response errcode:{0}, errmsg:{1}", errorCode.toString(), errorMessage);
 			throw new ErrorResponseException(errorCode, formatedErrorMessage);
 		}
 	}
